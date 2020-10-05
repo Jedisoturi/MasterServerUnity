7a86ab31e84e5d6476a983821f0d7b437a090030
Joona Eskonpekka <ejaa7991@gmail.com>
2020-10-05 12:02:27 +0300

    player script

---
+++ TJGame/Assets/Scripts/Network/GameStatus.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manages current game save status
+/// </summary>
+public class GameStatus : MonoBehaviour
+{
+    public static GameStatus _status;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (_status == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _status = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Save feeded data as game save
+    /// </summary>
+    /// <param name="data"></param>
+    public void SaveData(GameSaveData data)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(GameSaveFilePath());
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    /// <summary>
+    /// Load saved game data and return it to whom it may concern
+    /// </summary>
+    /// <returns></returns>
+    public GameSaveData LoadData()
+    {
+        if (File.Exists(GameSaveFilePath()))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(GameSaveFilePath(), FileMode.Open);
+            GameSaveData data = (GameSaveData)bf.Deserialize(file);
+            file.Close();
+
+            if (data._isRelevant)
+                return data;
+            else
+                return null;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Overrides saved game with isRelevant = false
+    /// </summary>
+    public void ResetData()
+    {
+        GameSaveData data = new GameSaveData();
+        data._isRelevant = false;
+        SaveData(data);
+    }
+
+    /// <summary>
+    /// Return path for saved file
+    /// </summary>
+    /// <returns></returns>
+    private string GameSaveFilePath()
+    {
+        return Application.persistentDataPath + "/playerInfo.dat";
+    }
+
+    public bool HasGameSaved()
+    {
+        GameSaveData data = LoadData();
+        if (data != null)
+            if (data._isRelevant)
+                return true;
+        return false;
+    }
+}
+
+[Serializable]
+public class GameSaveData
+{
+    public bool _isRelevant;
+    public Guid _guid;
+}
--- TJGame/Assets/Scripts/Network/Player.cs
+++ TJGame/Assets/Scripts/Network/Player.cs
@@ -2,15 +2,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
     public static Guid _guid;
+
+    private void Awake()
+    {
+        if (!GetPlayerFromFiles())
+        {
+            // ask for player name...
+        }
+    }
+
+    public static bool GetPlayerFromFiles()
+    {
+        if (GameStatus._status.HasGameSaved())
+        {
+            _guid = GameStatus._status.LoadData()._guid;
+            return true;
+        }
+        return false;
+    }
+
+    public static void SavePlayerToFiles()
+    {
+        var saveData = new GameSaveData();
+        saveData._isRelevant = true;
+        saveData._guid = _guid;
+        GameStatus._status.SaveData(saveData);
+    }
 
     public static void CreatePlayer(string name)
     {
         DBCreatePlayer(name);
+        SavePlayerToFiles();
+    }
+
+    public static void RemovePlayer()
+    {
+        GameStatus._status.ResetData();
     }
 
     #region DB
@@ -22,6 +55,11 @@ public class Player
 
         var player = JsonConvert.DeserializeObject<PlayerObject>(responseString);
         _guid = player.Id;
+    }
+
+    // TODO: Not implemented
+    public async static void DBRemovePlayer()
+    {
     }
 
     #endregion
