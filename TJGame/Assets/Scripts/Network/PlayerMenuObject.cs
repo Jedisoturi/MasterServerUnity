a4d1a2a1f1fb6451efbaf3be65ff25c81c28b72c
HenriLagerroos <43806027+HenriLagerroos@users.noreply.github.com>
2020-10-06 01:32:25 +0300

    Leaderboard

--- TJGame/Assets/Scripts/Network/PlayerMenuObject.cs
+++ TJGame/Assets/Scripts/Network/PlayerMenuObject.cs
@@ -9,11 +9,32 @@ public class PlayerMenuObject : MonoBehaviour
 {
     public TextMeshProUGUI name;
     public TextMeshProUGUI score;
+    public TextMeshProUGUI level;
+    public TextMeshProUGUI date;
+    public TextMeshProUGUI achievements;
     public Image background;
 
     public void UpdatePlayer(int index, PlayerObject player)
     {
-        name.text = index + ": " + player.Name;
-        score.text = "XP: " + player.Score.ToString();
+        name.text = (index + 1) + ". " + player.Name;
+        score.text = player.Score.ToString();
+        level.text = player.Level.ToString();
+        date.text = String.Format("{0:d/M/yyyy}", player.CreationTime.Date);
+        achievements.text = "";
+        int trueCount = 0;
+        for(int i = 0; i < player.Achievements.Length; i++)
+        {
+            if(player.Achievements[i] == true)
+            {
+                if (trueCount > 0)
+                    achievements.text += " | ";
+                trueCount++;
+                achievements.text += Enum.GetName(typeof(Achievement), i);
+            }
+
+        }
+
+        if (trueCount == 0)
+            achievements.text = "<color=grey>None</color>";
     }
 }
--- TJGame/Assets/Scripts/Network/ServerMenuObject.cs
+++ TJGame/Assets/Scripts/Network/ServerMenuObject.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class ServerMenuObject : MonoBehaviour
 {
+    public bool isDetail = false;
     public TextMeshProUGUI name;
     public TextMeshProUGUI endpoint;
     public TextMeshProUGUI players;
@@ -23,11 +24,23 @@ public class ServerMenuObject : MonoBehaviour
 
     public void UpdateServer(ServerObject server)
     {
-        serverObj = server;
-        name.text = server.Name;
-        endpoint.text = server.EndPoint;
-        players.text = server.Players.Count + " / " + server.MaxPlayers;
-        hasPass.text = server.HasPassword ? "On" : "Off";
+        if (isDetail)
+        {
+            serverObj = server;
+            name.text = server.Name;
+            endpoint.text = "Address: " + server.EndPoint;
+            players.text = "Player Count: " + server.Players.Count + " / " + server.MaxPlayers;
+            hasPass.text = "Password Protection: " + (server.HasPassword ? "On" : "Off");
+        }
+        else
+        {
+            serverObj = server;
+            name.text = server.Name;
+            endpoint.text = server.EndPoint;
+            players.text = server.Players.Count + " / " + server.MaxPlayers;
+            hasPass.text = (server.HasPassword ? "On" : "Off");
+        }
+
     }
 
     public void ViewDetails()
