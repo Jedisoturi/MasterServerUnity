77dd88336a2e34bf07c704e3ec8c8c57294b0f38
Joona Eskonpekka <ejaa7991@gmail.com>
2020-10-06 23:30:19 +0300

    signature lahetys kaikkialla

--- TJGame/Assets/Scripts/Network/Constants.cs
+++ TJGame/Assets/Scripts/Network/Constants.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class Constants
 {
     public const string apiAddress = "http://localhost:5000/";
+    public static readonly byte[] secret = Encoding.UTF8.GetBytes("8EB6F8D11A6D3F42813B2C43DD6C8".ToLower());
 }
