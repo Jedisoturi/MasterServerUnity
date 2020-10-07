8a818c7fc68532eb7558cbc32cf694eb4c002fb3
HenriLagerroos <43806027+HenriLagerroos@users.noreply.github.com>
2020-10-07 03:50:52 +0300

    player edit menu

--- TJGame/Assets/Scripts/Network/Network_UI/MenuObjects/PlayerMenuObject.cs
+++ TJGame/Assets/Scripts/Network/Network_UI/MenuObjects/PlayerMenuObject.cs
@@ -11,32 +11,59 @@ public class PlayerMenuObject : BaseMenuObject
     public TextMeshProUGUI levelText;
     public TextMeshProUGUI dateText;
     public TextMeshProUGUI achievementsText;
+    public bool isDetail = false;
 
     public override void UpdateObject(int index, BaseObject inObj)
     {
         PlayerObject player = (PlayerObject)inObj;
         if (player == null)
             return;
-
-        nameText.text = (index + 1) + ". " + player.Name;
-        scoreText.text = player.Score.ToString();
-        levelText.text = player.Level.ToString();
-        dateText.text = String.Format("{0:d/M/yyyy}", player.CreationTime.Date);
-        achievementsText.text = "";
-        int trueCount = 0;
-        for(int i = 0; i < player.Achievements.Length; i++)
+        if (!isDetail)
         {
-            if(player.Achievements[i] == true)
+            nameText.text = (index + 1) + ". " + player.Name;
+            scoreText.text = player.Score.ToString();
+            levelText.text = player.Level.ToString();
+            dateText.text = String.Format("{0:d/M/yyyy}", player.CreationTime.Date);
+            achievementsText.text = "";
+            int trueCount = 0;
+            for (int i = 0; i < player.Achievements.Length; i++)
             {
-                if (trueCount > 0)
-                    achievementsText.text += " | ";
-                trueCount++;
-                achievementsText.text += Enum.GetName(typeof(Achievement), i);
+                if (player.Achievements[i] == true)
+                {
+                    if (trueCount > 0)
+                        achievementsText.text += " | ";
+                    trueCount++;
+                    achievementsText.text += Enum.GetName(typeof(Achievement), i);
+                }
+
             }
 
+            if (trueCount == 0)
+                achievementsText.text = "<color=grey>None</color>";
         }
+        else
+        {
+            nameText.text = "Name: " + player.Name;
+            scoreText.text = "Score: " + player.Score.ToString();
+            levelText.text = "Level: " + player.Level.ToString();
+            dateText.text = "Creation time: " + player.CreationTime.ToString();
+            achievementsText.text = "Achievements: \n";
+            int trueCount = 0;
+            for (int i = 0; i < player.Achievements.Length; i++)
+            {
+                if (player.Achievements[i] == true)
+                {
+                    if (trueCount > 0)
+                        achievementsText.text += " | ";
+                    trueCount++;
+                    achievementsText.text += Enum.GetName(typeof(Achievement), i);
+                }
 
-        if (trueCount == 0)
-            achievementsText.text = "<color=grey>None</color>";
+            }
+
+            if (trueCount == 0)
+                achievementsText.text = "Achievements: \nNone";
+        }
+        
     }
 }
--- TJGame/Assets/Scripts/Network/Network_UI/UIManager.cs
+++ TJGame/Assets/Scripts/Network/Network_UI/UIManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -24,6 +25,8 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private GameObject _leaderMenu;
     [SerializeField]
+    private GameObject _playerMenu;
+    [SerializeField]
     private MenuListCreator _leaderMenuPlayerCreator;
     [SerializeField]
     private MenuListCreator _detailsMenuPlayerCreator;
@@ -34,6 +37,16 @@ public class UIManager : MonoBehaviour
     private GameSaveData playerData;
     private bool userCreated = false;
     private PlayerSort playerSort = PlayerSort.ScoreDesc;
+    [SerializeField]
+    private InputField _inputName;
+    [SerializeField]
+    private InputField _inputScore;
+    [SerializeField]
+    private InputField _inputLevel;
+    [SerializeField]
+    private TMP_Dropdown _inputAchievement;
+    [SerializeField]
+    private PlayerMenuObject playerMenuObject;
 
     [SerializeField]
     private MenuListCreator serverMenuList;
@@ -94,6 +107,7 @@ public class UIManager : MonoBehaviour
         _navigationMenu.SetActive(true);
         _leaderMenu.SetActive(false);
         _connectMenu.SetActive(true);
+        _playerMenu.SetActive(false);
         UpdateServerList();
     }
 
@@ -102,7 +116,17 @@ public class UIManager : MonoBehaviour
         _connectMenu.SetActive(false);
         _detailsMenu.SetActive(false);
         _leaderMenu.SetActive(true);
+        _playerMenu.SetActive(false);
         UpdatePlayerList();
+    }
+
+    public void OpenPlayerMenu()
+    {
+        _connectMenu.SetActive(false);
+        _detailsMenu.SetActive(false);
+        _leaderMenu.SetActive(false);
+        _playerMenu.SetActive(true);
+        UpdatePlayerItem();
     }
 
     public void CloseMenus()
@@ -110,6 +134,7 @@ public class UIManager : MonoBehaviour
         _connectMenu.SetActive(false);
         _navigationMenu.SetActive(false);
         _detailsMenu.SetActive(false);
+        _playerMenu.SetActive(false);
     }
 
     public void CreateUser()
@@ -155,6 +180,49 @@ public class UIManager : MonoBehaviour
         Client._instance.ConnectToServer(server);
     }
 
+    public void EditPlayer()
+    {
+        SendPlayer();
+    }
+
+    public async void SendPlayer()
+    {
+        string baseRequest = $"{Constants.apiAddress}api/players/{Player._guid}/";
+
+        if (!string.IsNullOrEmpty(_inputName.text))
+        {
+            string nameRequest = "rename/" + _inputName.text;
+            string request = baseRequest + nameRequest;
+            var response = await Player.PostAsync(request, null);
+            Debug.Log(request);
+        }
+
+        if(Int32.Parse(_inputScore.text) > 0)
+        {
+            string nameRequest = "incScore/" + Int32.Parse(_inputScore.text);
+            string request = baseRequest + nameRequest;
+            var response = await Player.PostAsync(request, null);
+            Debug.Log(request);
+        }
+
+        if (Int32.Parse(_inputLevel.text) > 0)
+        {
+            string nameRequest = "incLevel/" + Int32.Parse(_inputLevel.text);
+            string request = baseRequest + nameRequest;
+            var response = await Player.PostAsync(request, null);
+            Debug.Log(request);
+        }
+
+        if (_inputAchievement.value > 0)
+        {
+            string nameRequest = "addAchievement/" + (_inputAchievement.value - 1);
+            string request = baseRequest + nameRequest;
+            var response = await Player.PostAsync(request, null);
+            Debug.Log(request);
+        }
+        UpdatePlayerItem();
+    }
+
     public void RefreshServerList()
     {
         UpdateServerList();
@@ -172,6 +240,21 @@ public class UIManager : MonoBehaviour
             playerSort = (PlayerSort)sorting;
             UpdatePlayerList();
         }
+    }
+
+    private async void UpdatePlayerItem()
+    {
+        string request = $"{Constants.apiAddress}api/players/{Player._guid}";
+
+        var response = await Client._instance._httpClient.GetAsync(request);
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        PlayerObject playerObj = JsonConvert.DeserializeObject<PlayerObject>(responseString);
+
+        Debug.Log(responseString);
+
+        _inputName.text = playerObj.Name;
+        playerMenuObject.UpdateObject(0, playerObj);
     }
 
     private async void UpdatePlayerList()
